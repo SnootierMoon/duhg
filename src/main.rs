fbd1f82660947343c487b9da8926e670c6e9f72d
Akshay Trivedi <aku24.7x3@gmail.com>
2023-07-02 19:13:00 -0700

    camera

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use glam::Vec3A;
+use glam::{Mat4, Quat, Vec2, Vec3, Vec3A};
+use hexasphere::shapes::IcoSphere;
 use noise::{NoiseFn, Perlin};
 use rand::Rng;
+use std::time::{Duration, Instant};
 use wgpu::util::DeviceExt;
+use winit::event::{DeviceEvent, ElementState};
+use winit::window::CursorGrabMode;
 use winit::{
-    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    dpi::PhysicalSize,
+    event::{Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
 
 struct State {
     window: Window,
-    size: winit::dpi::PhysicalSize<u32>,
+    size: PhysicalSize<u32>,
+    move_keys_held: [bool; 6],
+
+    camera: Camera,
+    time: Instant,
+    delta_time: Duration,
 
     surface: wgpu::Surface,
     device: wgpu::Device,
@@ -19,14 +29,29 @@ struct State {
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
     index_buffer: wgpu::Buffer,
-
-    num_vertices: u32,
+    camera_buffer: wgpu::Buffer,
+    camera_bind_group: wgpu::BindGroup,
     num_indices: u32,
 }
 
 impl State {
     async fn new(window: Window) -> Self {
         let size = window.inner_size();
+        window.set_cursor_visible(false);
+
+        let camera = Camera {
+            pos: Vec3A::new(0.0, 2.0, 0.0),
+            forward: Vec3A::new(1.0, 0.0, 0.0),
+            pitch: 0.0,
+
+            speed: 0.001,
+            sens: 0.01,
+
+            aspect_ratio: size.width as f32 / size.height as f32,
+            fov_y_radians: std::f32::consts::FRAC_PI_4,
+            z_near: 0.05,
+            z_far: 20.0,
+        };
 
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             backends: wgpu::Backends::all(),
@@ -74,11 +99,80 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        let sphere = IcoSphere::new(10, |_| ());
+
+        let min_radius = 0.7f32;
+        let max_radius = 1.0f32;
+
+        let n = Perlin::new(rand::thread_rng().gen());
+
+        let vertices = sphere
+            .raw_points()
+            .iter()
+            .map(|point| {
+                let height = (n.get((point.as_dvec3() * 3.0).to_array()) / 2.0 + 0.5) as f32;
+                let radius = (max_radius - min_radius) * height + min_radius;
+                let point = *point * radius;
+                Vertex {
+                    position: [point.x, point.y, point.z],
+                    color: [height, height, height],
+                }
+            })
+            .collect::<Vec<_>>();
+
+        let indices = sphere
+            .get_all_indices()
+            .into_iter()
+            .map(|x| x as u16)
+            .collect::<Vec<_>>();
+
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(&vertices),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(&indices),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
+        let camera_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Camera Buffer"),
+            contents: bytemuck::cast_slice(&camera.matrix().to_cols_array()),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let camera_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("Camera Bind Group Layout"),
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }],
+            });
+
+        let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Camera Bind Group"),
+            layout: &camera_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: camera_buffer.as_entire_binding(),
+            }],
+        });
+
         let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
+                bind_group_layouts: &[&camera_bind_group_layout],
                 push_constant_ranges: &[],
             });
 
@@ -118,51 +212,13 @@ impl State {
             multiview: None,
         });
 
-        let sphere = hexasphere::shapes::IcoSphere::new(10, |_| ());
-
-        let center = Vec3A::new(0.0, 0.0, 0.0);
-        let min_radius = 0.6f32;
-        let max_radius = 1.0f32;
-
-        let n = Perlin::new(rand::thread_rng().gen());
-
-        let vertices = sphere
-            .raw_points()
-            .iter()
-            .map(|point| {
-                let height = (n.get(point.as_dvec3().to_array()) / 2.0 + 0.5) as f32;
-                println!("{height}");
-                let radius = (max_radius - min_radius) * height + min_radius;
-                let point = (center + *point) * radius;
-
-                Vertex {
-                    position: [point.x, point.y, point.z],
-                    color: [height, height, height],
-                }
-            })
-            .collect::<Vec<_>>();
-
-        let indices = sphere
-            .get_all_indices()
-            .into_iter()
-            .map(|x| x as u16)
-            .collect::<Vec<_>>();
-
-        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Vertex Buffer"),
-            contents: bytemuck::cast_slice(&vertices),
-            usage: wgpu::BufferUsages::VERTEX,
-        });
-
-        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Index Buffer"),
-            contents: bytemuck::cast_slice(&indices),
-            usage: wgpu::BufferUsages::INDEX,
-        });
-
         Self {
             window,
             size,
+            move_keys_held: [false; 6],
+            camera,
+            time: Instant::now(),
+            delta_time: Duration::ZERO,
             surface,
             device,
             queue,
@@ -170,30 +226,65 @@ impl State {
             render_pipeline,
             vertex_buffer,
             index_buffer,
-            num_vertices: vertices.len() as u32,
+            camera_buffer,
+            camera_bind_group,
             num_indices: indices.len() as u32,
         }
     }
 
-    fn window(&self) -> &Window {
-        &self.window
-    }
-
-    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+    pub fn resize(&mut self, new_size: PhysicalSize<u32>) {
         if new_size.width > 0 && new_size.height > 0 {
             self.size = new_size;
             self.config.width = new_size.width;
             self.config.height = new_size.height;
+            self.camera.aspect_ratio = new_size.width as f32 / new_size.height as f32;
             self.surface.configure(&self.device, &self.config);
         }
     }
 
-    #[allow(unused_variables)]
-    fn input(&mut self, event: &WindowEvent) -> bool {
-        false
+    fn move_key(&self, key: VirtualKeyCode) -> Option<usize> {
+        match key {
+            VirtualKeyCode::W => Some(0),
+            VirtualKeyCode::S => Some(1),
+            VirtualKeyCode::A => Some(2),
+            VirtualKeyCode::D => Some(3),
+            VirtualKeyCode::Space => Some(4),
+            VirtualKeyCode::LShift => Some(5),
+            _ => None,
+        }
     }
 
-    fn update(&mut self) {}
+    fn update(&mut self) {
+        let new_time = Instant::now();
+        self.delta_time = new_time.duration_since(self.time);
+        self.time = new_time;
+        let mut move_vec = Vec3A::ZERO;
+        if self.move_keys_held[0] {
+            move_vec.x += 1.0;
+        }
+        if self.move_keys_held[1] {
+            move_vec.x -= 1.0;
+        }
+        if self.move_keys_held[2] {
+            move_vec.y += 1.0;
+        }
+        if self.move_keys_held[3] {
+            move_vec.y -= 1.0;
+        }
+        if self.move_keys_held[4] {
+            move_vec.z += 1.0;
+        }
+        if self.move_keys_held[5] {
+            move_vec.z -= 1.0;
+        }
+        self.camera.update_pos(move_vec);
+
+        self.queue.write_buffer(
+            &self.camera_buffer,
+            0,
+            bytemuck::cast_slice(&[self.camera.matrix().to_cols_array()]),
+        )
+    }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
@@ -226,6 +317,7 @@ impl State {
         });
 
         render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.set_bind_group(0, &self.camera_bind_group, &[]);
         render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
         render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
         render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
@@ -236,10 +328,121 @@ impl State {
 
         Ok(())
     }
+
+    fn run(mut self, event_loop: EventLoop<()>) {
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(physical_size)
+                | WindowEvent::ScaleFactorChanged {
+                    new_inner_size: &mut physical_size,
+                    ..
+                } => {
+                    self.resize(physical_size);
+                }
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state,
+                            virtual_keycode: Some(key),
+                            ..
+                        },
+                    ..
+                } => {
+                    if let Some(id) = self.move_key(key) {
+                        self.move_keys_held[id] = state == ElementState::Pressed;
+                    }
+                }
+                _ => {}
+            },
+            Event::DeviceEvent { event, .. } => match event {
+                DeviceEvent::MouseMotion { delta, .. } => {
+                    self.camera
+                        .update_dir(Vec2::new(-delta.0 as f32, -delta.1 as f32));
+                }
+                _ => {}
+            },
+            Event::RedrawRequested(window_id) if window_id == self.window.id() => {
+                self.update();
+                match self.render() {
+                    Ok(_) => {}
+                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                        self.resize(self.size)
+                    }
+                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                }
+            }
+            Event::RedrawEventsCleared => {
+                self.window.request_redraw();
+            }
+            _ => {}
+        })
+    }
+}
+
+struct Camera {
+    pos: Vec3A,
+    forward: Vec3A,
+    pitch: f32, // [-pi/2, pi/2]
+
+    speed: f32,
+    sens: f32,
+
+    aspect_ratio: f32,
+    fov_y_radians: f32,
+    z_near: f32,
+    z_far: f32,
+}
+
+impl Camera {
+    fn matrix(&self) -> Mat4 {
+        let up = self.pos.normalize();
+        let (sin, cos) = self.pitch.sin_cos();
+        let center = self.pos + self.forward * cos + self.pos.normalize() * sin;
+        let view = Mat4::look_at_rh(self.pos.into(), center.into(), up.into());
+        let proj = Mat4::perspective_rh(
+            self.fov_y_radians,
+            self.aspect_ratio,
+            self.z_near,
+            self.z_far,
+        );
+        proj * view
+    }
+    // forward, left, up
+    fn update_pos(&mut self, delta: Vec3A) {
+        let delta = delta * self.speed;
+        let left = self.pos.cross(self.forward).normalize();
+        let h_move = delta.x * self.forward + delta.y * left;
+        let rot_axis = self.pos.cross(h_move).normalize_or_zero();
+        let rot = Quat::from_axis_angle(rot_axis.into(), h_move.length() * self.pos.length_recip());
+        self.pos = rot * self.pos;
+        self.forward = (rot * self.forward).reject_from(self.pos).normalize();
+        self.pos += self.pos.normalize() * delta.z;
+    }
+
+    // left, up
+    fn update_dir(&mut self, delta: Vec2) {
+        let delta = delta * self.sens;
+        self.pitch = (self.pitch + delta.y).clamp(
+            -std::f32::consts::FRAC_PI_2 + 0.001,
+            std::f32::consts::FRAC_PI_2 - 0.001,
+        );
+        let rot = Quat::from_axis_angle(self.pos.normalize().into(), delta.x);
+        self.forward = (rot * self.forward).normalize();
+    }
 }
 
 #[repr(C)]
-#[derive(Copy, Clone, Debug, Default, bytemuck::Pod, bytemuck::Zeroable)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
     position: [f32; 3],
     color: [f32; 3],
@@ -262,51 +465,5 @@ fn main() {
         .build(&event_loop)
         .unwrap();
 
-    pollster::block_on(async {
-        let mut state = State::new(window).await;
-
-        event_loop.run(move |event, _, control_flow| match event {
-            Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == state.window().id() => {
-                if !state.input(event) {
-                    match event {
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            input:
-                                KeyboardInput {
-                                    state: ElementState::Pressed,
-                                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                                    ..
-                                },
-                            ..
-                        } => *control_flow = ControlFlow::Exit,
-                        WindowEvent::Resized(physical_size) => {
-                            state.resize(*physical_size);
-                        }
-                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                            state.resize(**new_inner_size);
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
-                state.update();
-                match state.render() {
-                    Ok(_) => {}
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                        state.resize(state.size)
-                    }
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
-                }
-            }
-            Event::RedrawEventsCleared => {
-                state.window().request_redraw();
-            }
-            _ => {}
-        });
-    })
+    pollster::block_on(async { State::new(window).await.run(event_loop) });
 }
